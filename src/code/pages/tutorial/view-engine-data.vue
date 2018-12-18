8d705f015c5e43f03d72b5129b23846e84655fa0
Travis CI <ujjwalkumargupta44@gmail.com>
2018-12-18 04:17:33 +0000

    Deploy to GitHub Pages: ce3fa83d419a37c8340a9b809fb69eac1f9e4210

--- src/code/pages/tutorial/view-engine-data.vue
+++ src/code/pages/tutorial/view-engine-data.vue
@@ -10,7 +10,7 @@ import Tutorial from "../../components/tutorial.vue";
   }
 })
 export default class extends Vue {
-  tutorialContent = `%3Cp%3Etype%20ViewEngineData%20is%20used%20by%20ViewEngine%20to%20get%20view%20related%20data%20from%20workers.%3C/p%3E%0A%3Cpre%3E%3Ccode%3Etype%20ViewEngineData%20=%20%7B%0A%20%20%20%20view:%20string;%0A%20%20%20%20model:%20any;%0A%20%20%20%20extra?:%20any;%0A%7D%0A%3C/code%3E%3C/pre%3E`;
+  tutorialContent = `%3Cp%3EThe%20type%20ViewEngineData%20is%20used%20by%20ViewEngine%20to%20get%20view%20related%20data%20from%20workers.%3C/p%3E%0A%3Cpre%3E%3Ccode%3Etype%20ViewEngineData%20=%20%7B%0A%0A%20%20%20%20/**%0A%20%20%20%20%20*%20name%20of%20the%20view%20or%20path%20of%20view%0A%20%20%20%20%20*%0A%20%20%20%20%20*%20@type%20%7Bstring%7D%0A%20%20%20%20%20*/%0A%20%20%20%20view:%20string;%0A%0A%20%20%20%20/**%0A%20%20%20%20%20*%20view%20model%0A%20%20%20%20%20*%0A%20%20%20%20%20*%20@type%20%7B*%7D%0A%20%20%20%20%20*/%0A%20%20%20%20model:%20any;%0A%7D;%0A%3C/code%3E%3C/pre%3E`;
   title="View Engine Data";
 }
 </script>
