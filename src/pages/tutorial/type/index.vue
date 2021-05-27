030680e7cb09823d31f499a9c0e2fd6c0f39710c
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-05-27 08:50:45 +0530

    delete pages

--- src/pages/tutorial/type/index.vue
+++
@@ -1,7 +0,0 @@
-<template><Layout title='Types in fortjs' description='fortjs application options and details' keywords='types, configuration. fortjs, node' contentSrc='/home/warrior/projects/opensource/fortjs.docs/content/tutorial/type/index.md'></Layout></template>
-        <script>import Layout from '/home/warrior/projects/opensource/fortjs.docs/layouts/tutorial.vue'
-        export default {
-            components:{Layout}
-        };
-        </script>
-        
