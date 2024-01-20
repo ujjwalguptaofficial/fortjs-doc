eff0332d6c8d02091f9ced70eb1da3f2411f2b8a
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2024-01-20 20:56:25 +0400

    add hero title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h2 className="hero__title">{siteConfig.title}</h2>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
@@ -40,7 +40,7 @@ export default function Home() {
     >
       <div className='hero'>
         <div style={{ margin: '0 100px', marginTop: "-30px", marginBottom: "-30px" }}>
-          <h1>Revolutionizing Node.js Development</h1>
+          <h1 className='hero__title'>Revolutionizing Node.js Development</h1>
           <div style={{ fontWeight: "400", fontSize: "18px" }}>
             <p>
               While Node.js is undeniably powerful, its available frameworks and ecosystem often lead developers to focus solely on building the application, neglecting considerations for long-term maintainability and code cleanliness. The resulting codebase can be challenging for new team members to grasp.
