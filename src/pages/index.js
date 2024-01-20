091704da3084cc2c3121061af3a8d9444f828153
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2024-01-20 20:30:26 +0400

    add author's note

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,24 @@ export default function Home() {
       description="Fortjs is a feature-rich Node.js web framework designed for building powerful, scalable, and maintainable web applications. Explore its flexibility, simplicity, and extensive capabilities."
       keywords="Fort.js, Nestjs, Node.js, Web Framework, JavaScript, TypeScript, MVC, REST, HTTP, Web Development, Scalable Applications"
     >
+      <div className='hero'>
+        <div style={{ margin: '0 100px' }}>
+          <h1>Revolutionizing Node.js Development</h1>
+          <div style={{ fontWeight: "400", fontSize: "18px" }}>
+            <p>
+              While Node.js is undeniably powerful, its available frameworks and ecosystem often lead developers to focus solely on building the application, neglecting considerations for long-term maintainability and code cleanliness. The resulting codebase can be challenging for new team members to grasp.
+            </p>
+            <p>
+              In contrast, ecosystems like Java and C# offer robust frameworks that facilitate the creation of more maintainable applications. These ecosystems enable new employees to work on the product with relative ease, even without an in-depth understanding of the entire codebase.
+            </p>
+            <p>
+              Fort.js aims to address this issue by leveraging modern JavaScript and TypeScript. It provides simple APIs and adopts the Fort architecture, which promotes modularity through components without compromising performance (reportedly 3 times faster than Express.js).
+            </p>
+          </div>
+        </div>
+      </div>
       <HomepageHeader />
+
       <main>
         <HomepageFeatures />
       </main>
